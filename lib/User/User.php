b2fe89786fe0077544c38b7dc4c790ed38637468
Juan Pablo Villafáñez <jvillafanez@solidgear.es>
2017-04-19 14:33:54 +0200

    Add secondary check if email can't be set to null

--- lib/User/User.php
+++ lib/User/User.php
@@ -452,7 +452,11 @@ class User {
 						$user->setEMailAddress($email);
 					} catch (\Doctrine\DBAL\Exception\UniqueConstraintViolationException $ex) {
 						\OCP\Util::writeLog('user_ldap', 'can\'t set email [' . $email .'] for user ['. $this->uid .']. Trying to set as empty email', \OCP\Util::WARN);
-						$user->setEMailAddress(null);
+						try {
+							$user->setEMailAddress(null);
+						} catch (\Doctrine\DBAL\Exception\UniqueConstraintViolationException $ex) {
+							\OCP\Util::writeLog('user_ldap', 'can\'t set email [' . $email .'] nor making it empty for user ['. $this->uid .']', \OCP\Util::ERROR);
+						}
 					}
 				}
 			}
