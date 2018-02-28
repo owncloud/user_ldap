969872ba7d5ca2629f80e47268a420d306fce2b5
Jörn Friedrich Dreyer <jfd@butonic.de>
2018-02-28 11:23:17 +0100

    Treat emptystring userquota is valid value

--- lib/User/UserEntry.php
+++ lib/User/UserEntry.php
@@ -211,7 +211,10 @@ class UserEntry {
 			\OC::$server->getLogger()->debug("No LDAP quota attribute configured", ['app' => 'user_ldap']);
 		} else {
 			$quota = $this->getAttributeValue ($attr);
-			if (!$this->verifyQuotaValue($quota)) {
+			if ($quota === '') {
+				\OC::$server->getLogger()->debug("No quota set for <{$this->getOwnCloudUID()}>", ['app' => 'user_ldap']);
+				$quota = null;
+			} else if (!$this->verifyQuotaValue($quota)) {
 				\OC::$server->getLogger()->error("Invalid quota <$quota> for LDAP user <{$this->getOwnCloudUID()}>", ['app' => 'user_ldap']);
 				$quota = null;
 			}
