236896cf367dad50ec896eb814ee3878c3ab6546
Felix Heidecke <felix@heidecke.me>
2018-03-14 12:26:28 +0100

    Add basic ajax to load config

--- js/app.js
+++ js/app.js
@@ -4,11 +4,35 @@ var user_ldap = new Vue({
         app : {
             name: "User LDAP Settings … or whatever",
             description: "I love all the vue's!"
+        },
+        config : {
+            data    : null,
+            loading : false,
+            failed  : false
         }
     },
+    mounted: function() {
+        this.fetchConfig()
+    },
     methods: {
-        letsShout: function(string) {
-            alert(string);
+        fetchConfig: function() {
+            var ref = this;
+
+            this.config.loading = true;
+            this.config.failed  = false;
+
+            $.get(OC.generateUrl('apps/user_ldap/ajax/getConfiguration.php'))
+            .done(function(data) {
+
+                if (data.status != "success") {
+                    ref.config.loading = false;
+                    ref.config.failed  = true;
+                }
+                else {
+                    ref.config.data    = data.configuration;
+                    ref.config.loading = false;
+                }
+            })
         }
     }
 })
