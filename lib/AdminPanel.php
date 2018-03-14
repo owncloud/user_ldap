3d26f458f610e6178ae73bef37267e45b964f6e6
Felix Heidecke <felix@heidecke.me>
2018-03-14 12:21:00 +0100

    Remove php partials in template

--- lib/AdminPanel.php
+++ lib/AdminPanel.php
@@ -31,50 +31,11 @@ class AdminPanel implements ISettings {
 	public function getPanel() {
 		// fill template
 		$tmpl = new Template('user_ldap', 'settings');
-
-		$prefixes = $this->helper->getServerConfigurationPrefixes();
-		$hosts = $this->helper->getServerConfigurationHosts();
-
-		$wizardHtml = '';
-		$toc = [];
-
-		$wControls = new Template('user_ldap', 'part.wizardcontrols');
-		$wControls = $wControls->fetchPage();
-		$sControls = new Template('user_ldap', 'part.settingcontrols');
-		$sControls = $sControls->fetchPage();
-
-		$wizTabs = [];
-		$wizTabs[] = ['tpl' => 'part.wizard-server',      'cap' => $this->l->t('Server')];
-		$wizTabs[] = ['tpl' => 'part.wizard-userfilter',  'cap' => $this->l->t('Users')];
-		$wizTabs[] = ['tpl' => 'part.wizard-loginfilter', 'cap' => $this->l->t('Login Attributes')];
-		$wizTabs[] = ['tpl' => 'part.wizard-groupfilter', 'cap' => $this->l->t('Groups')];
-		$wizTabsCount = \count($wizTabs);
-		for ($i = 0; $i < $wizTabsCount; $i++) {
-			$tab = new Template('user_ldap', $wizTabs[$i]['tpl']);
-			if ($i === 0) {
-				$tab->assign('serverConfigurationPrefixes', $prefixes);
-				$tab->assign('serverConfigurationHosts', $hosts);
-			}
-			$tab->assign('wizardControls', $wControls);
-			$wizardHtml .= $tab->fetchPage();
-			$toc['#ldapWizard'.($i+1)] = $wizTabs[$i]['cap'];
-		}
-
-		$tmpl->assign('tabs', $wizardHtml);
-		$tmpl->assign('toc', $toc);
-		$tmpl->assign('settingControls', $sControls);
-
-		// assign default values
-		$config = new Configuration($this->config, '', false);
-		$defaults = $config->getDefaults();
-		foreach ($defaults as $key => $default) {
-			$tmpl->assign($key.'_default', $default);
-		}
-
 		return $tmpl;
 	}
 
 	public function getSectionID() {
 		return 'authentication';
 	}
+
 }
