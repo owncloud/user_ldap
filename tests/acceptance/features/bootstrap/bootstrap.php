f1371944f39e55afbdead8a2ebc37ec0e69f21cd
Phil Davis <phil@jankaritech.com>
2018-03-21 16:01:42 +0545

    Refactor acceptance tests for core changes

--- tests/acceptance/features/bootstrap/bootstrap.php
+++ tests/acceptance/features/bootstrap/bootstrap.php
@@ -23,20 +23,16 @@
 require __DIR__ . '/../../../../vendor/autoload.php';
 require __DIR__ . '/../../../../../../lib/base.php';
 require __DIR__ . '/../../../../../../lib/composer/autoload.php';
-require __DIR__ . '/../../../../../../tests/acceptance/features/bootstrap/WebUIBasicStructure.php';
-require __DIR__ . '/../../../../../../tests/acceptance/features/bootstrap/WebUIGeneralContext.php';
-require __DIR__ . '/../../../../../../tests/acceptance/features/bootstrap/WebUILoginContext.php';
-require __DIR__ . '/../../../../../../tests/acceptance/features/bootstrap/WebUISharingContext.php';
-require __DIR__ . '/../../../../../../tests/acceptance/features/bootstrap/WebUIFilesContext.php';
-require __DIR__ . '/../../../../../../tests/acceptance/features/bootstrap/WebUIUserContext.php';
-require __DIR__ . '/../../../../../../tests/acceptance/features/lib/OwncloudPage.php';
 
 $classLoader = new \Composer\Autoload\ClassLoader();
+$classLoader->addPsr4(
+	"", __DIR__ . "/../../../../../../tests/acceptance/features/bootstrap", true
+);
 $classLoader->addPsr4("Page\\", __DIR__ . "/../lib", true);
 $classLoader->addPsr4(
-	"Page\\", __DIR__ . "/../../../../../../tests/acceptance/features/lib/", true
+	"Page\\", __DIR__ . "/../../../../../../tests/acceptance/features/lib", true
 );
 $classLoader->addPsr4(
-	"TestHelpers\\", __DIR__ . "/../../../../../../tests/TestHelpers/", true
+	"TestHelpers\\", __DIR__ . "/../../../../../../tests/TestHelpers", true
 );
 $classLoader->register();
