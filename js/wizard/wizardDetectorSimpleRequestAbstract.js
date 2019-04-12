60f08bddb6ef6dabccf3b6f14a57343082eaaa51
Victor Dubiniuk <victor.dubiniuk@gmail.com>
2019-04-12 14:29:20 +0300

    Rename ldap_serverconfig_chooser into id and ldap_clear_mapping into subject

--- js/wizard/wizardDetectorSimpleRequestAbstract.js
+++ js/wizard/wizardDetectorSimpleRequestAbstract.js
@@ -34,7 +34,7 @@ OCA = OCA || {};
 			model.notifyAboutDetectionStart(this.targetKey);
 			var params = OC.buildQueryString({
 				action: this.wizardMethod,
-				ldap_serverconfig_chooser: configID
+				id: configID
 			});
 			return model.callWizard(params, this.processResult, this);
 		}
