a6496eda6847e1525d3e5db58057fb199dca7f8c
Victor Dubiniuk <victor.dubiniuk@gmail.com>
2019-04-30 12:05:14 +0300

    Deduplicate defaults

--- appinfo/Migrations/Version20190315192717.php
+++ appinfo/Migrations/Version20190315192717.php
@@ -140,6 +140,8 @@ class Version20190315192717 implements ISimpleMigration {
 		'ldap_paging_size'                  => 'ldapPagingSize',
 		'ldap_experienced_admin'            => 'ldapExperiencedAdmin',
 		'ldap_dynamic_group_member_url'     => 'ldapDynamicGroupMemberURL',
+		'ldap_uuid_user_attribute'          => 'ldapUuidUserAttribute',
+		'ldap_uuid_group_attribute'         => 'ldapUuidGroupAttribute'
 	];
 
 	/**
@@ -279,9 +281,10 @@ class Version20190315192717 implements ISimpleMigration {
 
 	/**
 	 * @param  mixed $value
-	 * @return string[]
+	 * @return string | string[]
 	 */
 	private function convertToArray($value) {
+		// try to split the value by all known separators
 		if (empty($value)) {
 			$value = '';
 		} elseif (!\is_array($value)) {
@@ -292,8 +295,10 @@ class Version20190315192717 implements ISimpleMigration {
 		}
 
 		if (!\is_array($value)) {
+			// if the value is not an array - store it as is
 			$finalValue = \trim($value);
 		} else {
+			// if the value is an array - clean all empty values
 			$finalValue = [];
 			foreach ($value as $key => $val) {
 				if (\is_string($val)) {
