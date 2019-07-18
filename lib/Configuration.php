4599ab65fed256f0b0f976e17ae763fa44c87de9
Victor Dubiniuk <victor.dubiniuk@gmail.com>
2019-07-18 17:07:47 +0300

    Revert "Kill isDefault"
    This reverts commit fd6ab1fef32805e3191a3493e4624c3ca3a956f4.

--- lib/Configuration.php
+++ lib/Configuration.php
@@ -243,6 +243,18 @@ class Configuration {
 		);
 	}
 
+	public function isDefault() {
+		$defaults = $this->getDefaults();
+		foreach ($this->getTranslatedConfig() as $key=>$value) {
+			if (\array_key_exists($key, $defaults) === false
+				|| $defaults[$key] !== $value
+			) {
+				return false;
+			}
+		}
+		return true;
+	}
+
 	/**
 	 * Sets multi-line values as arrays
 	 *
