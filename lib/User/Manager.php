3cb55e6369d25aeaeac3fe81c22bc2b543d6a6ea
Carlos Damken <carlos@damken.com>
2021-03-29 18:28:19 +0200

    Typo
    Found a small typo

--- lib/User/Manager.php
+++ lib/User/Manager.php
@@ -295,7 +295,7 @@ class Manager {
 			return true;
 		} catch (\OutOfBoundsException $e) {
 			// UUID attribute can't be detected this means that we need to drop the user on sync
-			$this->logger->info("DN '$dn' does not exist any more and can not be resolved using uuid attribute", ['app' => 'user_ldap']);
+			$this->logger->info("DN '$dn' does not exist anymore and can not be resolved using uuid attribute", ['app' => 'user_ldap']);
 		}
 		return false;
 	}
