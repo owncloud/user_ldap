e6d87166261e012f1c3098a3c481657e4e7e55dd
Juan Pablo Villafáñez <jvillafanez@solidgeargroup.com>
2021-09-07 13:23:20 +0200

    Include comments

--- lib/Helper.php
+++ lib/Helper.php
@@ -193,6 +193,7 @@ class Helper {
 		}
 
 		if (isset($param['hasBeenHandled']) && $param['hasBeenHandled']) {
+			// if the event has been handled already, ignore it
 			return;
 		}
 
@@ -208,7 +209,7 @@ class Helper {
 		$uid = $userBackend->loginName2UserName($param['uid']);
 		if ($uid !== false) {
 			$param['uid'] = $uid;
-			$param['hasBeenHandled'] = true;
+			$param['hasBeenHandled'] = true;  // mark the event as handled
 		}
 	}
 
